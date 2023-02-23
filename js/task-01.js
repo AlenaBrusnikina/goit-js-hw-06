21d56fe3855d38c5bbdb344bb8ce1b7682c1df3d
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-02-23 21:38:53 +0200

    task-01

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+const numberCategoriesEl = document.querySelectorAll('.item');
+const number = numberCategoriesEl.length;
+
+console.log(`Number of categories: ${number}`);
+
+const categoriesEl = numberCategoriesEl.forEach( element => {
+    const category = element.firstElementChild.textContent;
+    const elements = element.lastElementChild.children.length;
+
+    console.log(`Category: ${category}`)
+    console.log(`Elements: ${elements}`)            
+    }
+)
+
+
+
+
+
+
+
+
+
+
