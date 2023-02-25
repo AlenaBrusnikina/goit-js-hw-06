6c3f9d403319ff6962c262ce8a7a84bc58b628ba
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-02-25 22:52:18 +0200

    task-06

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const formInputEl = document.querySelector('#validation-input');
+
+
+formInputEl.addEventListener('blur', (event) => {
+
+    if (event.target.value.length === formInputEl.getAttribute('data-length')) {
+        event.currentTarget.classList.add('valid');
+        event.currentTarget.classList.remove("invalid")
+    }
+    else {
+        event.currentTarget.classList.remove("valid");
+        event.currentTarget.classList.add("invalid");
+      }
+});
