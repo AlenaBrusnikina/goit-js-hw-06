566aa232a826e7c56137637a84f4c81f149a1334
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-02-27 16:50:36 +0200

    task-05

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,6 @@ const defaultValue = 'Anonymous';
 const formInputEl = document.querySelector('#name-input');
 const formTextEl = document.querySelector('#name-output');
 
-formInputEl.addEventListener('input', (event) => {
-    formTextEl.textContent = event.target.value.trim() === '' ? defaultValue : event.target.value;
+formInputEl.addEventListener('input', event => {
+  formTextEl.textContent = event.target.value.trim() === '' ? defaultValue : event.target.value;
 });
-
