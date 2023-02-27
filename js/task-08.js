2fa74d35c5585e993cf0c3c0b3b09cc8ea6cdd86
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-02-27 19:47:54 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+formEl.reset();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email === '' || password === '') {
+    return alert('Всі поля повинні бути заповнені!');
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  event.target.reset();
+}
