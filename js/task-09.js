dc7448adf079a3565635e23d966d19daca9500ae
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-02-27 16:50:54 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  containerEl: document.querySelector('body'),
+  buttonChangeColorEl: document.querySelector('.change-color'),
+  spanColorEl: document.querySelector('.color'),
+};
+
+refs.buttonChangeColorEl.addEventListener('click', changeBackgroundColor);
+
+function changeBackgroundColor() {
+  let colorBody = getRandomHexColor();
+
+  refs.containerEl.style.backgroundColor = colorBody;
+  refs.spanColorEl.textContent = colorBody;
+}
