0fa2044e0eacb9c1db011d9f1bd8d077a9f4d234
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-02-28 22:38:23 +0200

    task -10

--- js/task-10.js
+++ js/task-10.js
@@ -2,4 +2,35 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const inputEl = document.querySelector('input');
+const dataCreat = document.querySelector('[data-create]');
+const dataDestroy = document.querySelector('[data-destroy]');
+const boxEl = document.querySelector('#boxes');
 
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement('div');
+    const size = 30 + i * 10;
+    const color = getRandomHexColor();
+
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = color;
+
+    boxEl.append(div);
+  }
+}
+
+const handleClick = () => {
+  createBoxes(inputEl.value);
+};
+
+dataCreat.addEventListener('click', handleClick);
+
+const deletesBox = () => {
+  boxEl.innerHTML = '';
+};
+
+dataDestroy.addEventListener('click', () => {
+  boxEl.innerHTML = '';
+});
