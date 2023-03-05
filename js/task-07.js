5d49b05831c68e892aa0950535a73e11001727d1
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-03-05 19:17:24 +0200

    task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,12 @@
+//! Пошук елементів
 const inputRangeEl = document.querySelector('#font-size-control');
 const textTitleEl = document.querySelector('#text');
 
-
+//! Додавання слухача подій
 inputRangeEl.addEventListener('input', (event) => {
     const inputValue = event.target.value;
     
+    //!Зміна розміру тексту
     textTitleEl.style.fontSize = `${inputValue}px`;
     console.log(textTitleEl);
 
