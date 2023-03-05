5697ac51d5694a1b39fa5752b23071d3596f63d2
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-03-05 19:17:06 +0200

    task-04

--- js/task-04.js
+++ js/task-04.js
@@ -1,13 +1,20 @@
+//! Ініціалізація лічильника
 let counterValue = 0;
+
+// !Пошук елементів
 const decrementBtn = document.querySelector('[data-action="decrement"]');
 const incrementBtn = document.querySelector('[data-action="increment"]');
 const myValue = document.querySelector('#value');
 
+
+//! Додавання слухача до кнопки
 decrementBtn.addEventListener('click', () => {
   counterValue -= 1;
   myValue.textContent = counterValue;
 });
 
+
+//! Додавання слухача до кнопки
 incrementBtn.addEventListener('click', () => {
   counterValue += 1;
   myValue.textContent = counterValue;
