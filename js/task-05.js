c16cea4d5e3da9695725988028aece4a41c9ef8d
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-03-05 19:17:11 +0200

    task-05

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,12 @@
+//! Пошук елементів
 const defaultValue = 'Anonymous';
 const formInputEl = document.querySelector('#name-input');
 const formTextEl = document.querySelector('#name-output');
+
+
+//! Додавання слухача події до інпута
+//! Присвоюємо поточне значення інпута в span
+//! Якщо інпут не заповнений то показати 'Anonymous'
 
 formInputEl.addEventListener('input', event => {
   formTextEl.textContent = event.target.value.trim() === '' ? defaultValue : event.target.value;
