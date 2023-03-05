67b0a40932434973295d181695f07a272f2349f4
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-03-05 19:16:46 +0200

    task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,19 @@
+//!Пошук елементів
 const numberCategoriesEl = document.querySelectorAll('.item');
 const number = numberCategoriesEl.length;
 
+//!Виведення кількості категорій в консоль через .length
 console.log(`Number of categories: ${number}`);
 
+
+//! Перебирання кожного елемента/дитини з .item
 const categoriesEl = numberCategoriesEl.forEach(element => {
   const category = element.firstElementChild.textContent;
   const elements = element.lastElementChild.children.length;
 
+   //! Виведення тексту із кожного заголовку
   console.log(`Category: ${category}`);
+
+   //! Виведення кількості елементів
   console.log(`Elements: ${elements}`);
 });
