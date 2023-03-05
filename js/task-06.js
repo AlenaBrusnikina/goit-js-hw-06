44cb61f09d493ec37777b6dd2cfd52008de766a8
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-03-05 19:17:18 +0200

    task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,13 @@
+//! Пошук поля введення
 const formInputEl = document.querySelector('#validation-input');
 
+//! Додаємо слухача подій
 formInputEl.addEventListener('blur', event => {
   const valueLength = event.target.value.trim();
   console.log(valueLength.length);
   const dataLength = formInputEl.getAttribute('data-length');
 
+   //! Перевірка на число 6
   if (valueLength.length == dataLength) {
     formInputEl.classList.add('valid');
     formInputEl.classList.remove('invalid');
