95947efdf2c996d9940eb55f81fd8660e3e76477
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-03-05 19:17:37 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -2,6 +2,8 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+
+//!Пошук елементів
 const refs = {
   containerEl: document.querySelector('body'),
   buttonChangeColorEl: document.querySelector('.change-color'),
@@ -11,8 +13,8 @@ const refs = {
 refs.buttonChangeColorEl.addEventListener('click', changeBackgroundColor);
 
 function changeBackgroundColor() {
-  let colorBody = getRandomHexColor();
+  let colorBody = getRandomHexColor();  //! Рандомний колір кожен раз
 
-  refs.containerEl.style.backgroundColor = colorBody;
-  refs.spanColorEl.textContent = colorBody;
+  refs.containerEl.style.backgroundColor = colorBody; //! Фарбуємо бекграунд в рандомний колір
+  refs.spanColorEl.textContent = colorBody;  //! Прописуємо назву кольору в елемент span
 }
