ac3069b53560a07b8c8baa37bb286bb9c579d475
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-03-05 19:16:53 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,12 +1,16 @@
+
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
 
+
+//! Перебір кожного елементу
 const ingredientEl = ingredients.map(element => {
-  const elementList = document.createElement('li');
-  elementList.classList.add('item');
-  elementList.textContent = element;
+  const elementList = document.createElement('li'); //! Створення елементу списку в розмітці
+  elementList.classList.add('item'); //!Додавання класу до кожного елементу
+  elementList.textContent = element; //! Додавання текстового контенту до кожного елементу
 
   return elementList;
 });
 
+//! Пошук списку і додавання масиву з елементами в середину списка
 const itemEl = document.querySelector('#ingredients');
 itemEl.append(...ingredientEl);
